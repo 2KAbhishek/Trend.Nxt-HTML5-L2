a05f0f208dc049827eb196d1d429cbd7719c23e6
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-03-22 01:17:06 +0530

    Add project script

---
+++ script.js
@@ -0,0 +1,29 @@
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  let data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+navigator.geolocation.getCurrentPosition(showPosition, errorPosition);
+
+function showPosition(position) {
+  localStorage.setItem("lat", position.coords.latitude);
+  localStorage.setItem("lon", position.coords.longitude);
+  document.getElementById("location").innerHTML =
+    "Latitude : " +
+    localStorage.getItem("lat") +
+    "<br> Longitude : " +
+    localStorage.getItem("lon");
+}
+
+function errorPosition() {
+  document.getElementById("location").innerHTML = "Error in fetching location.";
+}
